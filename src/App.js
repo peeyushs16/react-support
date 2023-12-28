dd70a6e292c1f727de4981579407ec37f9763b48
peeyushs16 <peeyushs16@gmail.com>
2023-12-28 15:50:14 +0530

    Using of props & component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Hi react</div>
+    <div className="App">
+      <JobDetail name="Peeyush" salary={20000} position="PHP Dev" company="Konceive" />
+      <JobDetail name="Peeyush" salary={20000} position="PHP Dev" company="Konceive" />
+    </div>
   );
 }
 
+
+const JobDetail = (props) =>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
+}
 export default App;
